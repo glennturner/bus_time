0ae3cf6a41a2a6f5ccc147f45505be88b0f3a1ea
G Turner <contact@iamgturner.com>
2025-01-07 17:30:56 -0600

    Initial commit

---
+++ lib/bus_time/bullentin.rb
@@ -0,0 +1,7 @@
+module BusTime
+  class Bulletin
+    attr_reader :route_id, :stop_id, :stop_name :url, :updated_at
+    def initialize(route_id:, stop_id:, stop_name:, url:, updated_at:)
+    end
+  end
+end
---
+++ lib/bus_time/prediction.rb
@@ -0,0 +1,8 @@
+module BusTime
+  class Prediction
+    attr_reader :prediction_time, :prediction_type, :direction
+
+    def initialize(prediction_time, prediction_type:, direction:)
+    end
+  end
+end
