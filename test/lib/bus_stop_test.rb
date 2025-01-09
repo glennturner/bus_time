8e3818039fbfd2868fc9a40c1cc886c97526f505
G Turner <contact@iamgturner.com>
2025-01-09 14:25:33 -0600

    Initial commit

---
+++ test/lib/bus_stop_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class BusTime::BusStopTest < BusTime::BusTimeTest
+  test "if nearby stops" do
+    nearby_coords = [41.9809728, -87.6650963]
+
+    assert stop.nearby?(nearby_coords[0], nearby_coords[1])
+    assert !stop.nearby?(nearby_coords[0], nearby_coords[1] * 2)
+  end
+
+  private
+
+  def stop
+    BusTime::BusStop.new("1234", "Clark & Little Bad Wolf",
+                         coords: [41.9833784, -87.6689953],
+                         direction: "Southbound")
+  end
+end
