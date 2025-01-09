f22555eb01d7ffa744adacdf001884e729e06b43
G Turner <contact@iamgturner.com>
2025-01-08 23:34:58 -0600

    feat: complete initialize attr handling

--- lib/bus_time/prediction.rb
+++ lib/bus_time/prediction.rb
@@ -3,11 +3,25 @@
 # Service bus stop prediction handling, including prediction retrieval
 # via `BusTime::Api`
 class BusTime::Prediction
-  attr_reader :time, :prediction_type, :delay, :generated_at
+  attr_reader :time, :prediction_type, :delayed, :generated_at,
+              :stop_id, :stop_name, :direction
 
-  def initialize(arrives_at, delayed: false, generated_at: DateTime.now)
-    @arrives_at = arrives_at
-    @delayed = delayed
-    @generated_at = generated_at
+  def initialize(route_id, direction, stop_id, arrival_minutes, **opts)
+    @route_id = route_id
+    @stop_id = stop_id
+    @direction = direction
+    @arrival_minutes = arrival_minutes
+
+    @stop_name = opts[:stop_name]
+
+    @arrives_at = opts[:arrives_at]
+    @prediction_type = opts[:prediction_type] || "arrival"
+    @delayed = opts[:delayed] || false
+
+    @generated_at = opts[:generated_at] || DateTime.now
+  end
+
+  def delayed?
+    @delayed
   end
 end
